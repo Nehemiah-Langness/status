57e9a53e8948c6b018d33ef26c502e8664401b9f
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2025-06-18 10:18:55 -0500

    wrapped notification inside event.waitUntil

--- notifications.js
+++ notifications.js
@@ -6,22 +6,26 @@ self.addEventListener("push", async function (event) {
     return;
   }
 
-  const notifications = await self.registration.getNotifications();
+  const processNotification = async () => {
+    const notifications = await self.registration.getNotifications();
 
-  if (!data.doNotDisturb) {
-    notifications.forEach((notification) => notification.close());
-  } else if (!notifications.length) {
-    const title = data.title ?? "Do Not Disturb";
-    const body = data.body ?? "Nehemiah is currently in a meeting";
-    const tag = "do-not-disturb";
+    if (!data.doNotDisturb) {
+      notifications.forEach((notification) => notification.close());
+    } else if (!notifications.length) {
+      const title = data.title ?? "Do Not Disturb";
+      const body = data.body ?? "Nehemiah is currently in a meeting";
+      const tag = "do-not-disturb";
 
-    self.registration.showNotification(title, {
-      body: body,
-      icon: "/red.svg",
-      badge: "/mask.svg",
-      tag: tag,
-      renotify: false,
-      silent: false,
-    });
-  }
+      self.registration.showNotification(title, {
+        body: body,
+        icon: "/red.svg",
+        badge: "/mask.svg",
+        tag: tag,
+        renotify: false,
+        silent: false,
+      });
+    }
+  };
+
+  event.waitUntil(processNotification());
 });
